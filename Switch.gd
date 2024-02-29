604b9a8c3957aae71de35c73739ad8a7c2f6030c
Dr-Sirius <mcnew1721@gmail.com>
2024-02-28 23:36:38 -0500

    Implemented switch timer

--- Switch.gd
+++ Switch.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var swap_obj: Node3D
+@onready var switch_timer = %SwitchTimer
 
 var last_pos
 # Called when the node enters the scene tree for the first time.
@@ -9,8 +10,10 @@ func _ready():
 
 func _physics_process(delta):
 	last_pos = swap_obj.global_position
-	if Input.is_action_just_pressed("Switch Position"):
+	
+	if Input.is_action_just_pressed("Switch Position") and switch_timer.is_stopped():
 		swap_obj.global_position = global_position
 		global_position = last_pos
+		switch_timer.start()
 		
 		
