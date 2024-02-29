3ed7efe5a5f7808a1e0f8de237775cf1795d1f31
Dr-Sirius <mcnew1721@gmail.com>
2024-02-29 10:56:26 -0500

    Implemented switch obj following

--- Switch.gd
+++ Switch.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 @export var swap_obj: Node3D
+@export var move_with_obj: bool
+@export var offset: Vector3
 @onready var switch_timer = %SwitchTimer
 
 var last_pos
@@ -9,11 +11,15 @@ func _ready():
 	last_pos = swap_obj.global_position
 
 func _physics_process(delta):
+	
 	last_pos = swap_obj.global_position
 	
 	if Input.is_action_just_pressed("Switch Position") and switch_timer.is_stopped():
 		swap_obj.global_position = global_position
 		global_position = last_pos
+		offset *= -1
 		switch_timer.start()
-		
+	
+	if move_with_obj:
+		global_position = swap_obj.global_position + offset
 		
